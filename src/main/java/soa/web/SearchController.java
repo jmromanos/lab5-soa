f0cb1d4d5f0c366778b7d70bac7453497864741e
jmromanos <jmromanos96@gmail.com>
2017-12-19 12:50:58 +0100

    Added max:n limit

--- src/main/java/soa/web/SearchController.java
+++ src/main/java/soa/web/SearchController.java
@@ -1,15 +1,22 @@
 package soa.web;
 
 import org.apache.camel.ProducerTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 @Controller
 public class SearchController {
+
+    private static final Logger log = LoggerFactory.getLogger(SearchController.class);
 
     @Autowired
     private ProducerTemplate producerTemplate;
@@ -23,6 +30,14 @@ public class SearchController {
     @RequestMapping(value="/search")
     @ResponseBody
     public Object search(@RequestParam("q") String q) {
-        return producerTemplate.requestBodyAndHeader("direct:search", "", "CamelTwitterKeywords", q);
+        Map<String, Object> header = new HashMap<>();
+        if (q.contains("max:")) {
+            String n = q.substring(q.lastIndexOf("max:")+4);
+            header.put("CamelTwitterKeywords",q.substring(0,q.lastIndexOf("max:")-1));
+            header.put("CamelTwitterCount",n);
+        } else {
+            header.put("CamelTwitterKeywords",q);
+        }
+        return producerTemplate.requestBodyAndHeaders("direct:search", "",header);
     }
 }
